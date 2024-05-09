e6a1aed19bad4e7ea313e42ba930f675d8f84507
3mr353nturk <esukrusenturk@gmail.com>
2024-05-09 23:12:45 +0300

    commit

--- CaseProject/Program.cs
+++ CaseProject/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=News}/{action=Index}/{id?}");
 
 app.Run();
